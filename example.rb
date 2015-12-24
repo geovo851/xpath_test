091fc3b8cdc527fc9c49d28a05a367cae394d757
Georgy <geovo851@gmail.com>
2015-12-24 09:26:27 -0800

    Finish

---
+++ example.rb
@@ -0,0 +1,15 @@
+require 'rexml/document'
+
+f = File.open("xml/test.xml")
+xmldoc = REXML::Document.new(f)
+
+# Info for the first movie found
+movie = REXML::XPath.first(xmldoc, "//movie")
+p movie
+
+# Print out all the movie types
+REXML::XPath.each(xmldoc, "//type") { |e| puts e.text }
+
+# Get an array of all of the movie formats.
+names = REXML::XPath.match(xmldoc, "//format").map {|x| x.text }
+p names
---
+++ xpath_test.rb
@@ -0,0 +1,22 @@
+require 'xpath'
+require 'rexml/document'
+
+f = File.open("xml/test_1.xml")
+xmldoc = REXML::Document.new(f)
+
+i = 0
+els = []
+REXML::XPath.each(xmldoc, 'ul/ded//ul[@id="foo"]') do |el|
+  els << el
+  i += 1
+  puts i
+  puts el
+end
+
+puts '--------------------------------------------'
+
+els.each do |el|
+  el.each_element do |e|
+    puts e
+  end
+end
